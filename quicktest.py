b485d9402e725fcb8365f8d410fb1eb9f3dd2307
Konstantin Schubert <schubert.konstantin@gmail.com>
2015-02-11 00:31:26 +0100

    simplifications

--- quicktest.py
+++ quicktest.py
@@ -31,42 +31,10 @@ class QuickDjangoTest(object):
     
     def __init__(self, *args, **kwargs):
         self.apps = args
-        # Get the version of the test suite
-        self.version = self.get_test_version()
-        # Call the appropriate one
-        if self.version == 'new':
-            self._new_tests()
-        else:
-            self._old_tests()
+        self._tests()
+        
     
-    def get_test_version(self):
-        """
-        Figure out which version of Django's test suite we have to play with.
-        """
-        from django import VERSION
-        if VERSION[0] == 1 and VERSION[1] >= 2:
-            return 'new'
-        else:
-            return 'old'
-    
-    def _old_tests(self):
-        """
-        Fire up the Django test suite from before version 1.2
-        """
-        settings.configure(DEBUG = True,
-           DATABASE_ENGINE = 'sqlite3',
-           DATABASE_NAME = os.path.join(self.DIRNAME, 'database.db'),
-           INSTALLED_APPS = self.INSTALLED_APPS + self.apps
-        )
-        from django.test.simple import run_tests
-        failures = run_tests(self.apps, verbosity=1)
-        if failures:
-            sys.exit(failures)
-    
-    def _new_tests(self):
-        """
-        Fire up the Django test suite developed for version 1.2
-        """
+    def _tests(self):
         settings.configure(
             DEBUG = True,
             DATABASES = {
@@ -79,6 +47,9 @@ class QuickDjangoTest(object):
                     'PORT': '',
                 }
             },
+            SHIBBOLETH_LOGIN_URL = 'https://your_domain.edu/Shibboleth.sso/Login'
+	    SHIBBOLETH_LOGOUT_URL = 'https://your_domain.edu/Shibboleth.sso/Logout'
+	    LOGIN_URL = '/shib/login/'
             INSTALLED_APPS = self.INSTALLED_APPS + self.apps,
 	    ROOT_URLCONF = 'shib.urls',
         )
